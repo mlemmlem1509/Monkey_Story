49de05278ff2fc15dd93409619e3f2b947553778
mlemmlem1509 <deadontime69@gmail.com>
2023-08-11 10:36:51 +0700

    add user migration/seeder/model/ and AuthCotroller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PageContentSeeder::class);
         $this->call(ImageSeeder::class);
         $this->call(AudioSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
